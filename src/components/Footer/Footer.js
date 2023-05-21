ba81ebac590a5f14138242d4c0c47ee73ba89f12
Pavel <Pavel.Debchuk@gmail.com>
2023-05-21 22:16:17 +0300

    test

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,12 @@
-import React from "react";
-import "./FooterStyled.css";
+import React from 'react';
+import './FooterStyled.css';
 
 const Footer = () => {
-  return <div>
-  <p>pavel.debchuk@gmail.com</p>
-  </div>;
+  return (
+    <div className="footer">
+      <p>pavel.debchuk@gmail.com</p>
+    </div>
+  );
 };
 
 export default Footer;
