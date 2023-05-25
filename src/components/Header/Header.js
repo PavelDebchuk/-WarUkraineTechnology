af35aeb2aaf7c8c17295783e363ba1c2740e624d
Pavel <debchuk93@gmail.com>
2023-05-25 10:01:14 +0000

    add menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,12 +2,23 @@ import React from "react";
 import "./HeaderStyled.css";
 const Header = () => {
   return (
-    <ul>
-      <li>tanks</li>
-      <li>rszo</li>
-      <li>auto</li>
-    </ul>
-  );
-};
+    <header className="Header">
+       <h1 className="Header_title">stop war</h1>
+       <nav className="Header_nav">
+        <ul className="Header_list">
+          <li className="Header_item">
+            про війну
+          </li>
+          <li className="Menu_item">
+            зброя учасників
+          </li>
+          <li className="Menu_item">
+            auto
+          </li>
+        </ul>
+       </nav>
+    </header>
+  )
+}
 
 export default Header;
